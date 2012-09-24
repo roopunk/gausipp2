9437662ee4c63d1b8623aadb0fc479d5b77cd60e
Roopam Poddar <mailroopam@gmail.com>
2012-09-24 23:49:17 +0530

    CI files

---
+++ application/controllers/pages.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Pages extends CI_Controller {
+
+	public function index()
+	{
+		echo "<h2>Gausipp 2</h2>";
+	}
+}
+
+
